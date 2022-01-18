4d65c48daacb63bfdf5d121aa4dbf3501cf0516b
Alexander Batyrgariev <sasha@bashkort.org>
2022-01-19 04:30:37 +0500

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+requirements = [
+    "aiohttp>=3.7.4.post0,<4",
+    "websockets==9.1",
+    "pyhumps==1.6.1",
+    "web3==5.25.0",
+    "orjson==3.4.7"
+]
+
+setup(
+    name='bxcommon',
+    packages=['bxcommon', 'bxutils'],
+    package_dir={'': 'src'},
+    install_requires=requirements
+)
